8f66138334fa4f1d9710950d817772457e72aceb
budicuy <personal.budicuy@gmail.com>
2025-07-22 13:59:04 +0800

    feat: update Prisma schema and migration files; remove 'jam' column from Penumpang table and add necessary warnings

---
+++ prisma/migrations/20250722050031_remove_jam_column/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `jam` on the `Penumpang` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Penumpang" DROP COLUMN "jam";
